3847c22947447ec747252f322ce54a13f2883943
MKS-students <mks.students@hackreactor.com>
2015-08-03 14:50:58 -0500

    taxonomy complete

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,21 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age =10;
+  this.job = 'find pollen';
+  this.food = Bee.prototype.food;
+  this.canFly = true;
+  this.treasureChest =[];
+
+
 };
+
+
+ForagerBee.prototype = new Bee();
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.eat = Bee.prototype.eat;
+ForagerBee.prototype.forage = function (treasure){
+  this.treasureChest.push(treasure);
+}
+
+
 
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'make honey';
+  this.food = Bee.prototype.food;
+  this.honeyPot = 0;
+
+
+
 };
+HoneyMakerBee.prototype = new Bee();
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+HoneyMakerBee.prototype.eat = Bee.prototype.eat;
+HoneyMakerBee.prototype.makeHoney = function (){
+  this.honeyPot +=1;
+}
+HoneyMakerBee.prototype.giveHoney = function (){
+  this.honeyPot -=1;
+}
 
